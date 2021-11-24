bda7ebd64d359cec395a49c26155e7cc329e3256
Julian Wachholz <julian@wachholz.ch>
2021-11-24 16:26:17 +0100

    Provision the dashboard correctly

--- grafana.dockerfile
+++ grafana.dockerfile
@@ -1,4 +1,4 @@
-FROM grafana/grafana:8.2.3
+FROM grafana/grafana:8.2.5
 
-COPY grafana/data/dashboards/dokku-dash /var/lib/grafana/dashboards/dokku-dash
-COPY grafana/config/provisioning /etc/grafana/provisioning
+COPY grafana/dashboards/dokku-dash /etc/dashboards/dokku-dash
+COPY grafana/provisioning /etc/grafana/provisioning
--- prepare.sh
+++ prepare.sh
@@ -66,6 +66,7 @@ dokku network:set grafana attach-post-create dashboard
 dokku proxy:ports-set grafana http:80:3000 https:443:3000
 # Persistent storage
 dokku storage:ensure-directory grafana-data
+sudo chown -R 472:472 /var/lib/dokku/data/storage/grafana-data
 dokku storage:mount grafana /var/lib/dokku/data/storage/grafana-data:/var/lib/grafana
 # Deploy from dockerfile
 dokku builder-dockerfile:set grafana dockerfile-path grafana.dockerfile
